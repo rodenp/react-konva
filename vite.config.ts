7bad99965906d2876c31d78abeaed697a65dab17
rodenp <rodenp@hotmail.com>
2025-04-10 12:07:41 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
-  }
-})
+    open: true,
+    allowedHosts: ["khwfv7-5173.csb.app"],
+  },
+});
